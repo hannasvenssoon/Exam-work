28f6aa154d97882c76c25a30cfbe82404996799f
Hanna <hannas_svensson@hotmail.com>
2025-11-13 15:23:14 +0100

    Fördta commit

---
+++ read_file.py
@@ -0,0 +1,32 @@
+import serial
+import csv
+
+SERIAL_PORT = 'COM3'
+SERIAL_RATE = 115200
+FILE1 = "acc_liggande_data.csv"
+FILE2 = "acc_stående_data.csv"
+FILE3 = "acc_lutad_data.csv"
+FILE4 = "acc_rorelse_data.csv"
+
+def main():
+    ser = serial.Serial(SERIAL_PORT, SERIAL_RATE)
+
+    with open(FILE1, mode = 'a', newline = '') as file:
+        write = csv.writer(file)
+
+        print('Samlar data till ' + FILE1)
+        while True:
+            read = ser.readline().decode('utf-8').strip()
+
+            if read:
+                parts = read.split(',')
+                if len(parts) == 3:
+                    write.writerow(parts)
+                    file.flush()
+
+                print(read)
+
+
+
+if __name__ == "__main__":
+    main()
