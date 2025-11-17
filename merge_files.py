807e8a3f3794c70e9eca72411a4fb118ff950aaa
Hanna <hannas_svensson@hotmail.com>
2025-11-17 14:27:51 +0100

    merged dataset

--- merge_files.py
+++ merge_files.py
@@ -1,2 +1,15 @@
 import pandas as pd
 
+df = pd.concat(
+    [
+        pd.read_csv('acc_liggande_labeled.csv', sep = ';', names=["x", "y", "z", "label"]),
+        pd.read_csv('acc_stående_labeled.csv', sep = ';', names=["x", "y", "z", "label"]),
+        pd.read_csv('acc_rörelse_labeled.csv', sep = ';', names=["x", "y", "z", "label"]),
+                    
+    ], 
+    ignore_index = True
+)
+
+df.to_csv('acc_full_data.csv', index = False, sep = ';', header = False)
+
+print('Ny fil skapad')
